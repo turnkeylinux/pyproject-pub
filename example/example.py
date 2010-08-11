50c4b76855fb047c87058e6c1b66b8290b4e1589
Liraz Siri <liraz@sterilesecurity.com>
2010-08-11 07:50:14 +0300

    free software => open source software

--- example/example.py
+++ example/example.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-#
+# 
 # Copyright (c) 2010 Liraz Siri <liraz@turnkeylinux.org>
-#
+# 
 # This file is part of Example.
-#
-# Example is free software; you can redistribute it and/or
+# 
+# Example is open source software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 3 of
 # the License, or (at your option) any later version.
-#
+# 
 """Example command template
 
 Options:
